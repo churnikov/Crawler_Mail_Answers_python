65a9113d69d4c152c3508d86b0f7b913e00b663e
Nikita Churikov <Nikita@MacBook-Pro-Nikita.local>
2017-03-29 18:56:26 +0300

    - Created function to get categories - Created function to add items to database

--- src/Crawler.py
+++ src/Crawler.py
@@ -9,7 +9,8 @@ from bs4 import BeautifulSoup as bs
 class Crawler(object):
 
     def __init__(self, categories='all', timeline = 'all', verbose=True,
-                 schema_name='schema.sql', db_name='q_database.sqlt'):
+                 schema_name='schema.sql', db_name='q_database.sqlt',
+                 bs_features='lxml'):
         """
         init method for Crawler
         :params:
@@ -23,14 +24,20 @@ class Crawler(object):
                        -- default val:'schema.sql'
             db_name    -- (str)               -- name of database
                        -- default val:'q_database.sqlt'
+            bs_features-- (str)               -- BeautifulSoup engine to parse html page
+                          Look up https://www.crummy.com/software/BeautifulSoup/bs4/doc/ *Installing parser* section
+                          It explains things about parsers
+                          In short, if something goes wrong, change to 'html.parser'
+                       -- deafult val:'lxml'
         """
         self.categories = categories
         self.timeline = timeline
         self.verbose = verbose
         self.schema_name = schema_name
         self.db_name = db_name
+        self.bs_features=bs_features
 
-        self.__mail_page = 'https://touch.otvet.mail.ru/'
+        self.__mail_page = 'https://otvet.mail.ru/'
 
     def get_db(self):
         """Returns database if exist or creates one and returns it"""
@@ -41,7 +48,7 @@ class Crawler(object):
 
     def init_db(self):
         """Initilizes database with sql file"""
-        get_db()
+        self.get_db()
         with open(self.schema_name, 'r') as f:
             self.db.executescript(f.read())
         self.db.commit()
@@ -67,3 +74,42 @@ class Crawler(object):
             return r.text
         else:
             return None
+
+    def add_to_database(self, table, items):
+        """Add tuples from *items* to *table*"""
+        try:
+            c = self.db.cursor()
+            for item in items:
+                item_for_db = ','.join(item)
+                print(item_for_db)
+                c.execute('INSERT INTO {t} VALUES({i})'.format(t=table, i=item_for_db))
+            self.db.commit()
+        except:
+            raise sqlite3.Error('Unable to insert items into {}'.format(table))
+
+    def get_categories(self, add_to_db=True):
+        """
+        Downloads parent categories
+        :param: add_to_db -- (bool) -- if True will connect to database and add them
+                          -- default val:True
+        :returns: (list) -- list of tuples with (category_name, name of link)
+                            name_of_link: /example/
+        """
+        # getting main page
+        text_page = self.get_page()
+        soup = bs(text_page, self.bs_features)
+        # searching for categories
+        categories = soup.find_all('a', 'medium item item_link')
+        # transforming into list of tuples
+        # tuple := (name, web_name)
+        if self.categories != 'all':
+            cats_to_db = [(str(i), '\''+cat.text+'\'', '\''+cat['href']+'\'')
+                          for i, cat in enumerate(categories)
+                          if cat.text in self.categories]
+        else:
+            cats_to_db = [(str(i), '\''+cat.text+'\'', '\''+cat['href']+'\'')
+                          for i, cat in enumerate(categories)]
+
+        if add_to_db:
+            self.add_to_database(table='categories', items=cats_to_db)
+        return cats_to_db
--- src/schema.sql
+++ src/schema.sql
@@ -1,29 +1,29 @@
 CREATE TABLE IF NOT EXISTS questions(
   `id` INTEGER PRIMARY KEY,
-  `q_category_id` INTEGER NOT NULL
-  `q_sub_category_id` INTEGER
+  `q_category_id` INTEGER NOT NULL,
+  `q_sub_category_id` INTEGER,
   `q_title` TEXT NOT NULL,
-  `q_text` TEXT
-  FOREIGN KEY(`q_category_id`) REFERENCES categories(`id`)
+  `q_text` TEXT,
+  FOREIGN KEY(`q_category_id`) REFERENCES categories(`id`),
   FOREIGN KEY(`q_sub_category_id`) REFERENCES sub_categories(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS answers(
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `id` INTEGER PRIMARY KEY,
   `question_id` INTEGER NOT NULL,
   FOREIGN KEY(`question_id`) REFERENCES questions(`id`)
 );
 
 CREATE TABLE IF NOT EXISTS categories(
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `name` TEXT NOT NULL
-  `web_name` TEXT NOT NULL
+  `id` INTEGER PRIMARY KEY,
+  `name` TEXT UNIQUE NOT NULL,
+  `web_name` TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS sub_categories(
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `id` INTEGER PRIMARY KEY,
   `parent_cat_id` INTEGER,
-  `name` TEXT NOT NULL,
-  `web_name` TEXT NOT NULL
+  `name` TEXT UNIQUE NOT NULL,
+  `web_name` TEXT UNIQUE NOT NULL,
   FOREIGN KEY(`parent_cat_id`) REFERENCES categories(`id`)
 );
